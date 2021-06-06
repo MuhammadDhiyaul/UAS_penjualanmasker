21e67cfd9e11deaaa7218c100722bb1804123535
MuhammadDhiyaulAuliya <59078451+MuhammadDhiyaul@users.noreply.github.com>
2021-06-06 19:01:30 +0700

    UAS_penjualanmasker_Progres1

---
+++ resources/views/user/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('user.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            
+        </div>
+    </div>
+</div>
+@endsection
